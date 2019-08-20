f23e710915d58307039beb3e4aa91e8097d627dc
gabrielpda15 <gabriel.pda15@gmail.com>
2019-08-20 15:32:41 -0300

    Modificação no namespace padrão

--- GamesPardinho.Web/GamesPardinho.API/Program.cs
+++ GamesPardinho.Web/GamesPardinho.API/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace GamesPardinho.API
+namespace GamesPardinho.Web.API
 {
     public class Program
     {
