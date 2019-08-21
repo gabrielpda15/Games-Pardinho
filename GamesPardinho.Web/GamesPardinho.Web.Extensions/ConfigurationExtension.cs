befc6d3866193daeef6f66ca76cc3f1e208e4578
gabrielpda15 <gabriel.pda15@gmail.com>
2019-08-21 15:31:28 -0300

    Criação extensão para configuração

---
+++ GamesPardinho.Web/GamesPardinho.Web.Extensions/ConfigurationExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesPardinho.Web.Extensions
+{
+    public static class ConfigurationExtension
+    {
+        public static TConfig GetSection<TConfig>(this IConfiguration configuration)
+        {
+            return configuration.GetSection(typeof(TConfig).Name).Get<TConfig>();
+        }
+    }
+}
