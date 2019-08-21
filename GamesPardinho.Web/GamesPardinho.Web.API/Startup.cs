779d43f5887d0e250c5c14a1dd9e7bc75062dfd2
gabrielpda15 <gabriel.pda15@gmail.com>
2019-08-21 15:33:01 -0300

    Atualização das confugurações do startup

--- GamesPardinho.Web/GamesPardinho.Web.API/Startup.cs
+++ GamesPardinho.Web/GamesPardinho.Web.API/Startup.cs
@@ -1,20 +1,31 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using GamesPardinho.Web.Extensions;
+using GamesPardinho.Web.Models.Entities.Security;
+using GamesPardinho.Web.Models.Repository.Context;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace GamesPardinho.Web.API
 {
     public class Startup
     {
+        public const string CONN_STRING = "DefaultConnection";
+        public const string SCHEME = JwtBearerDefaults.AuthenticationScheme;
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -22,13 +33,47 @@ namespace GamesPardinho.Web.API
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddMvc();
+
+            var authConfig = Configuration.GetSection<AuthConfig>();
+            services.AddSingleton(authConfig);
+
+            services.AddDbContext<ModelDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString(CONN_STRING));
+            });
+
+            services.AddIdentity<Identity, Role>()
+                .AddEntityFrameworkStores<ModelDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = SCHEME;
+                options.DefaultChallengeScheme = SCHEME;
+                options.DefaultScheme = SCHEME;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.SaveToken = true;
+                options.RequireHttpsMetadata = false;
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidIssuer = authConfig.Issuer,
+                    ValidAudience = authConfig.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authConfig.SecretKey))
+                };
+            });
+
+
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,9 +82,8 @@ namespace GamesPardinho.Web.API
             }
 
             app.UseHttpsRedirection();
-
             app.UseRouting();
-
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
