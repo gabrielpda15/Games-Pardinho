55f151ef3d2c60e4a172868235f263184e17b6a1
Gabriel Pupim de Almeida <gabriel.pda15@gmail.com>
2019-08-22 08:25:03 -0300

    Inicio criação do repositorio base

---
+++ GamesPardinho.Web/GamesPardinho.Web.Models/Repository/Base/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GamesPardinho.Web.Models.Repository.Base
+{
+    public interface IRepository<TEntity> where TEntity : Entities.Base.BaseEntity
+    {
+        Task<IEnumerable<TEntity>> QueryAsync(IUserContext userContext, Expression<Func<TEntity, bool>> filter = null, Func<IQueryable<TEntity>, IQueryable<TEntity>> orderBy = null, CancellationToken ct = default);
+        Task<TEntity> QueryScalarAsync(IUserContext userContext, Expression<Func<TEntity, bool>> filter = null, CancellationToken ct = default);
+        Task<TEntity> QueryByIdAsync(int id, IUserContext userContext, CancellationToken ct = default);
+        Task<TEntity> AddAsync(IUserContext userContext, CancellationToken ct = default);
+    }
+}
---
+++ GamesPardinho.Web/GamesPardinho.Web.Models/Repository/Base/IUserContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesPardinho.Web.Models.Repository.Base
+{
+    public interface IUserContext
+    {
+    }
+}
