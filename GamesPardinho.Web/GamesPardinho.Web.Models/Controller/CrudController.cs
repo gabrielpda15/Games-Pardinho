6905c6a36e9cceaa7a756c90ce395ae4c4d6268c
Gabriel Pupim de Almeida <gabriel.pda15@gmail.com>
2019-08-22 21:19:03 -0300

    Finalização do CRUD Controller

--- GamesPardinho.Web/GamesPardinho.Web.Models/Controller/CrudController.cs
+++ GamesPardinho.Web/GamesPardinho.Web.Models/Controller/CrudController.cs
@@ -13,7 +13,7 @@ namespace GamesPardinho.Web.Models.Controller
 {
     [Authorize]
     [Route("api/[controller]")]
-    public abstract class CrudController<TEntity> : Microsoft.AspNetCore.Mvc.Controller where TEntity : class, IBaseEntity
+    public abstract class CrudController<TEntity> : GController where TEntity : class, IBaseEntity
     {
         protected IAsyncRepository<TEntity> Repository { get; }
         protected IUserContext UserContext { get; }
@@ -37,6 +37,7 @@ namespace GamesPardinho.Web.Models.Controller
         [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(Exception), StatusCodes.Status500InternalServerError)]
         [ProducesDefaultResponseType]
         public virtual async Task<IActionResult> GetById(int id, CancellationToken ct)
         {
@@ -44,13 +45,15 @@ namespace GamesPardinho.Web.Models.Controller
             {
                 return Ok(await Repository.QueryByIdAsync(id, UserContext, ct));
             }
-            catch { return NotFound(); }
+            catch (QueryException) { return NotFound(); }
+            catch (Exception ex) { return Internal(ex); }
         }
 
         [HttpPost]
         [ProducesResponseType(typeof(object), StatusCodes.Status201Created)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Exception), StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
         public virtual async Task<IActionResult> Post([FromBody]TEntity entity, CancellationToken ct)
         {
             try
@@ -58,7 +61,86 @@ namespace GamesPardinho.Web.Models.Controller
                 var obj = await Repository.AddAsync(entity, UserContext, ct);
                 return CreatedAtAction(nameof(Post), obj);
             }
-            catch { return BadRequest(); }
+            catch (Exception ex) { return BadRequest(ex); }
         }
+
+        [HttpPost("Multi")]
+        [ProducesResponseType(typeof(IEnumerable<object>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Exception), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public virtual async Task<IActionResult> PostAll([FromBody]IEnumerable<TEntity> entities, CancellationToken ct)
+        {
+            try
+            {
+                var obj = await Repository.AddAllAsync(entities, UserContext, ct);
+                return Ok(obj);
+            }
+            catch (Exception ex) { return BadRequest(ex); }
+        }
+
+        [HttpPut]
+        [ProducesResponseType(typeof(object), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Exception), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public virtual async Task<IActionResult> Put([FromBody]TEntity entity, CancellationToken ct)
+        {
+            try
+            {
+                return Ok(await Repository.UpdateAsync(entity, UserContext, ct));
+            }
+            catch (Exception ex) { return BadRequest(ex); }
+        }
+
+        [HttpPut("All")]
+        [ProducesResponseType(typeof(IEnumerable<object>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Exception), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public virtual async Task<IActionResult> PutAll([FromBody]IEnumerable<TEntity> entities, CancellationToken ct)
+        {
+            try
+            {
+                return Ok(await Repository.UpdateAllAsync(entities, UserContext, ct));
+            }
+            catch (Exception ex) { return BadRequest(ex); }
+        }
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(Exception), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
+        public virtual async Task<IActionResult> Delete(int id, CancellationToken ct)
+        {
+            try
+            {
+                if (await Repository.DeleteAsync(id, UserContext, ct)) return Ok();
+            }
+            catch (QueryException) { return NotFound(); }
+            catch (Exception ex) { return BadRequest(ex); }
+            return BadRequest();
+        }
+
+        [HttpDelete("All/{ids}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(Exception), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesDefaultResponseType]
+        public virtual async Task<IActionResult> DeleteAll(string ids, CancellationToken ct)
+        {
+            try
+            {
+                var result = await Repository.DeleteAllAsync(ids, UserContext, ct);
+                if (result == null) return Ok();
+                return Ok(result);
+            }
+            catch (FormatException ex) { return BadRequest(ex); }
+        }
+
     }
 }
---
+++ GamesPardinho.Web/GamesPardinho.Web.Models/Controller/GController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesPardinho.Web.Models.Controller
+{
+    public class GController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public ObjectResult Internal(object value)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError, value);
+        }
+
+        public StatusCodeResult Internal()
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+    }
+}
