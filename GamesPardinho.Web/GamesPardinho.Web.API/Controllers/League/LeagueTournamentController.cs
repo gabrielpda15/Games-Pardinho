6137a80a11398e4888147b5079863e7ae6e5bec1
Gabriel Pupim de Almeida <gabriel.pda15@gmail.com>
2019-08-23 12:29:29 -0300

    Adicionar novos controllers

---
+++ GamesPardinho.Web/GamesPardinho.Web.API/Controllers/League/LeagueTournamentController.cs
@@ -0,0 +1,21 @@
+﻿using GamesPardinho.Web.Models.Controller;
+using GamesPardinho.Web.Models.Entities.League;
+using GamesPardinho.Web.Models.Repository.Base;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GamesPardinho.Web.API.Controllers.League
+{
+    [Authorize]
+    [Route("api/League/[controller]")]
+    public class TournamentController : CrudController<LeagueTournament>
+    {
+        public TournamentController(IUnitOfWork unitOfWork, IUserContext userContext) : base(unitOfWork, userContext)
+        {
+        }
+    }
+}
