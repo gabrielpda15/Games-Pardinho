96cc021e3d30285482392c71bc6a9398b9b3a8c9
Gabriel Pupim de Almeida <gabriel.pda15@gmail.com>
2019-08-22 21:19:16 -0300

    Criação do RoleRepository

---
+++ GamesPardinho.Web/GamesPardinho.Web.Models/Repository/Repositories/Security/RoleRepository.cs
@@ -0,0 +1,17 @@
+﻿using GamesPardinho.Web.Models.Entities.Security;
+using GamesPardinho.Web.Models.Repository.Base;
+using GamesPardinho.Web.Models.Repository.Context;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesPardinho.Web.Models.Repository.Repositories.Security
+{
+    [Repository(typeof(Role))]
+    public class RoleRepository : BaseRepository<Role, ModelDbContext>
+    {
+        public RoleRepository(ModelDbContext context) : base(context)
+        {
+        }
+    }
+}
