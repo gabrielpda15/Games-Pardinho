20300118540ef55b910b0441f3a4f12946eabca4
gabrielpda15 <gabriel.pda15@gmail.com>
2019-08-28 11:28:02 -0300

    Criação do projeto de angular

---
+++ Angular/GamesPardinho/src/app/forms/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'login',
+  templateUrl: './login.component.html',
+  styleUrls: ['../forms.module.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
