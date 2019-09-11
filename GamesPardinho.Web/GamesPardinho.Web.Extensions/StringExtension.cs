2e5f8d39015f91eabca84d0e8dad9e57ae0e3c06
gabrielpda15 <gabriel.pda15@gmail.com>
2019-09-11 11:55:44 -0300

    Extensão para checagem de string vazia

---
+++ GamesPardinho.Web/GamesPardinho.Web.Extensions/StringExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GamesPardinho.Web.Extensions
+{
+    public static class StringExtension
+    {
+        public static bool IsNothing(this string str)
+        {
+            return string.IsNullOrEmpty(str) || string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
